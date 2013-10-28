af1025adfa1df3ba54d04372b2367d8e8825541b
maciej.nowicki <virtual92@gmail.com>
2013-10-28 17:00:54 +0100

    Add init interface and implementations for each low level standalone module

---
+++ frontend/standalone/modules/statistics/src/main/java/pl/lodz/pl/ftims/supermarket/frontend/standalone/statistics/core/Init.java
@@ -0,0 +1,13 @@
+package pl.lodz.pl.ftims.supermarket.frontend.standalone.statistics.core;
+
+import pl.lodz.pl.ftims.supermarket.frontend.standalone.core.Initalizable;
+
+public class Init implements Initalizable{
+
+	@Override
+	public void init() {
+		// TODO Init of statistics module
+		
+	}
+
+}
