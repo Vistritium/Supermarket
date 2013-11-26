fa22eed2d82b3160a7db46e4165b9794634bafbe
Mattanjah <djsnayper@gmail.com>
2013-11-26 11:28:10 +0100

    xaxa

--- backend/api/src/test/java/FinanceManagementTest.java
+++ backend/api/src/test/java/FinanceManagementTest.java
@@ -1,17 +1,47 @@
 
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import backend.api.FinanceManagement;
+import backend.core.model.Users;
 
 public class FinanceManagementTest {
 
+	@Ignore
 	@Test
 	public void test() {
 	
 		FinanceManagement financeManagement = new FinanceManagement();
 		SessionFactory instance = FinanceManagement.getInstance();
+		
+	}
+	
+	@Ignore
+	@Test
+	public void queryTest() {
+		
+		 Session s = FinanceManagement.getInstance().openSession();
+	        try {
+	             //pracownik = (Pracownik) s.load(Pracownik.class, id);
+
+	            Query query = s.createQuery("select u from Users u");
+	            
+	            List<Users> users =  query.list();
+	            
+	            for(Users user : users){
+	            	System.out.println(user);
+	            }
+
+	        } finally {
+	            s.close();
+	        }
+		
 		
 	}
 
